02de6109d12d783ff3b6d4839cda142b0f9a6525
khue001 <khue001@e.ntu.edu.sg>
2015-07-03 01:33:41 +0800

    * moving image with JQuery

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,7 +9,38 @@ angular.module('myApp.controller', [])
       };
         $scope.goToScan= function(){
             $state.go('scan');
+        };
+
+        function moveImage(){
+            var count = 0, //Work with actual number type
+                image = document.getElementById('movingImage'),
+                timerId = 0;
+            var rect = image.getBoundingClientRect();
+            console.log(rect.top, rect.right, rect.bottom, rect.left);
+            var top = rect.height;
+            timerId = setInterval( function() { //This function is called by the browser every 33 milliseconds
+                top += 50;
+                count++;
+                if( count == 5 ) {
+                    clearInterval( timerId ); //Stop the interval because left is > 200
+                }
+                var pixel = top + "px";
+                console.log("top: " + top + " pixel: " + pixel);
+               //angular.element('#movingImage').css('top' ,'pixel');
+               // angular.element.find("#movingImage")[0].style.top = pixel;
+                $("#movingImage").css("top" ,"pixel");
+            }, 1000 );
         }
+        //moveImage();
+        $(document).ready(function() {
+            $("#movingImage").animate({top: "+=350"}, 5000);
+            //$("#movingImage").animate({left: "-=300"}, 1000);
+        });
+
+        //$(document).ready(function() {
+        //    $("#").animate({left: "+=500"}, 2000);
+        //    $("#b").animate({left: "-=300"}, 1000);
+        //});
 
     });
 // A complex subclass of Parse.Object
