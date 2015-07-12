043d57f3e738b4021562c70f98c23320e95dfe35
khue001 <khue001@e.ntu.edu.sg>
2015-07-13 07:46:16 +0800

    * update table + animation

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,9 +6,60 @@ var start = 0;
 var totalScore = 0 ;
 var scanInterval;
 var truckInterval;
+//angular.module('ui.bootstrap.demo', ['ui.bootstrap']).controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+//
+//    $scope.items = items;
+//    $scope.selected = {
+//        item: $scope.items[0]
+//    };
+//
+//    $scope.ok = function () {
+//        $modalInstance.close($scope.selected.item);
+//    };
+//
+//    $scope.cancel = function () {
+//        $modalInstance.dismiss('cancel');
+//    };
+//});
+
+
 
 angular.module('myApp.controller', [])
-.controller('trialController', function($state, $scope) {
+    .controller('trialController',  function ($scope,  $state) {
+
+        //$scope.items = ['item1', 'item2', 'item3'];
+        //
+        //$scope.animationsEnabled = true;
+        //
+        //$scope.open = function (size) {
+        //
+        //    var modalInstance = $modal.open({
+        //        animation: $scope.animationsEnabled,
+        //        templateUrl: 'myModalContent.html',
+        //        controller: 'ModalInstanceCtrl',
+        //        size: size,
+        //        resolve: {
+        //            items: function () {
+        //                return $scope.items;
+        //            }
+        //        }
+        //    });
+        //
+        //    modalInstance.result.then(function (selectedItem) {
+        //        $scope.selected = selectedItem;
+        //    }, function () {
+        //        $log.info('Modal dismissed at: ' + new Date());
+        //    });
+        //};
+        //
+        //$scope.toggleAnimation = function () {
+        //    $scope.animationsEnabled = !$scope.animationsEnabled;
+        //};
+
+
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
 
 
 
@@ -63,20 +114,27 @@ angular.module('myApp.controller', [])
             console.log("Type: " + typeAlarm);
 
             setTimeout(function(){
-                console.log("Callback when truck end here");
+                console.log("Start new setTimeout here");
                 notifyTruckFull();
+
+                //$("#blue")
+                //    .animate( {backgroundColor:'#fce3ac'}, 6 * 1000);
+                $('tr')
+                    .animate( {backgroundColor:'#fce3ac'}, 6 * 1000);
+                    //.animate({
+                    //    'background-color': '#0000FF'
+                    //}, truckTime * 1000, function () {
+                    //    console.log("Callback when truck end here: animate color");
+                    //    notifyTruckFull();
+                    //});
+                    //.delay(10 * 10000)
+                    //.animate({
+                    //    backgroundColor : '#FFFFFF'
+                    //}, 10);
+
             }, truckTime * 1000);
 
-            $(".progress-bar")
-                .animate({
-                    width: "100%"
-                }, truckTime * 1000, function () {
-                    console.log("Callback when truck end here");
-                    notifyTruckFull();
-                }).delay(10 * 10000)
-                .animate({
-                    width: "0%"
-                }, 10);
+
 
             if( typeAlarm === 1 ){
                 var alarmTimeout = setTimeout(function(){
@@ -236,3 +294,4 @@ var Monster = Parse.Object.extend("Monster", {
 //var monster = Monster.spawn(200);
 //alert(monster.get('strength'));  // Displays 200.
 //alert(monster.sound); // Displays Rawr.
+
