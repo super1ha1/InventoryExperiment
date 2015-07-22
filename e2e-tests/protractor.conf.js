28d05e39762fb5cec9aec92af62e8963b3e876e0
Nguyen Dac Khue <khue0011@gmail.com>
2015-07-22 18:12:37 +0800

    *test with protractor

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -1,19 +1,23 @@
+//exports.config = {
+//  allScriptsTimeout: 11000,
+//
+//  specs: [
+//    '*.js'
+//  ],
+//
+//  capabilities: {
+//    'browserName': 'chrome'
+//  },
+//
+//  baseUrl: 'http://localhost:8000/app/',
+//
+//  framework: 'jasmine',
+//
+//  jasmineNodeOpts: {
+//    defaultTimeoutInterval: 30000
+//  }
+//};
 exports.config = {
-  allScriptsTimeout: 11000,
-
-  specs: [
-    '*.js'
-  ],
-
-  capabilities: {
-    'browserName': 'chrome'
-  },
-
-  baseUrl: 'http://localhost:8000/app/',
-
-  framework: 'jasmine',
-
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
-  }
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['todo-spec.js']
 };
